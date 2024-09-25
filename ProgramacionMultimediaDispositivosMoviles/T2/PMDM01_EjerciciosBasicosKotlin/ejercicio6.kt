1f64e77df89efe3fff39ea570bede9084f325bc7
Oscar Montero <94902365+OscarW1ld3@users.noreply.github.com>
2024-09-25 21:10:45 +0200

    a

---
+++ ProgramacionMultimediaDispositivosMoviles/T2/PMDM01_EjerciciosBasicosKotlin/ejercicio6.kt
@@ -0,0 +1,17 @@
+/**
+ * Bucles indeterminados: while y do while
+ */
+fun main() {
+	val str: String = "zapatos"
+    var count: Int = 0;
+    
+    for (i in 0..str.length-1) {
+        var c: Char = str.get(i)
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++
+        }
+    }
+    
+    println("Hay " + count + " vocales")
+    
+}
