ce1734c295da9b5f3bd9280762505c31bcfb9872
OscarW1ld3 <94902365+OscarW1ld3@users.noreply.github.com>
2024-10-23 22:36:56 +0200

    a

--- DesarrolloInterfaces/Desarrollo/01-intro-typescript/src/topics/06-desest-funciones.ts
+++ DesarrolloInterfaces/Desarrollo/01-intro-typescript/src/topics/06-desest-funciones.ts
@@ -1,23 +1,24 @@
-export interface Producto {
-	descripcion: string;
-	precio: number;
+interface Producto {
+    descripcion: string;
+    precio: number;
 }
 
 const telefono: Producto = {
-	descripcion: 'iPhone 15 Pro',
-	precio: 1000
-}
+    descripcion: 'iPhone 15 Pro',
+    precio: 1000
+};
 
 const tablet: Producto = {
-	descripcion: 'iPad Air 3',
-	precio: 500
-}
+    descripcion: 'iPad Air 3',
+    precio: 500
+};
 
 interface CalculaImpuestoOpciones {
-	impuesto: number;
-	productos: Producto[];
+    impuesto: number;
+    productos: Producto[];
 }
 
+/*
 function calculaImpuesto (opciones: CalculaImpuestoOpciones): number[] {
 	let total = 0;
 	opciones.productos.forEach(producto => {
@@ -25,21 +26,36 @@ function calculaImpuesto (opciones: CalculaImpuestoOpciones): number[] {
 	});
 	return [total, total * opciones.impuesto];
 }
+ */
+// He cambiado para que reciba parámetros desestructurando
+function calculaImpuesto({ impuesto, productos }: CalculaImpuestoOpciones): number[] {
+    let total = 0;
+    productos.forEach(({ precio }) => {
+        total += precio;
+    });
+    return [total, total * impuesto];
+}
 
 const carritoCompra = [telefono, tablet];
 const impuesto = 0.21;
 
+/*
 const resultado = calculaImpuesto({
 	productos: carritoCompra,
 	impuesto: impuesto
 	// también podemos poner por la línea anterior, ya que se llaman iguales:
 	// impuesto
 })
+*/
+// Desestructuración al acceder a un dato en una estructura de control
+const [total, totalImpuesto] = calculaImpuesto({productos: carritoCompra, impuesto});
 
-console.log('Total',resultado[0]);
-console.log('Impuesto',resultado[1]);
+console.log('Total', total);
+console.log('Impuesto', totalImpuesto);
 
-const[total1, ] = resultado;
-const[, impuesto1] = resultado;
+// He cambiado para que se acceda al dato desestructurando
+for (const { descripcion, precio } of carritoCompra) {
+    console.log(`Producto: ${descripcion}, Precio: ${precio}`);
+}
 
 export {};
