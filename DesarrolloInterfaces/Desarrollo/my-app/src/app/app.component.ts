319614cd3d208cda833ccd0581f3f3a90c78565d
Oscar Montero <94902365+OscarW1ld3@users.noreply.github.com>
2024-11-07 18:08:24 +0100

    a

--- DesarrolloInterfaces/Desarrollo/my-app/src/app/app.component.ts
+++ DesarrolloInterfaces/Desarrollo/my-app/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ ContadorComponent ],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent
+{
+  public title: string = 'Mi Aplicacion';
+  public contador: number = 0;
+
+  // Si se le pasa el valor 0, resetea el contador
+  cambiarPor(x: number): void
+  {
+    if (x==0)
+      x = this.contador * -1
+    this.contador += x;
+  }
 }
---
+++ DesarrolloInterfaces/Desarrollo/my-app/src/app/contador/contador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-contador',
+  template: '<h1>Hola Mundo</h1>',
+  standalone: true,
+})
+export class ContadorComponent
+{
+
+}
