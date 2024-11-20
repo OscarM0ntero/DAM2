962fa3a53eba8224ec3968ceeb1d5703ee05d403
OscarW1ld3 <wilde.txt@gmail.com>
2024-11-20 15:00:27 +0100

    a

--- DesarrolloInterfaces/Desarrollo/listado-centros/src/app/app.component.ts
+++ DesarrolloInterfaces/Desarrollo/listado-centros/src/app/app.component.ts
@@ -1,10 +1,12 @@
+
 import { Component } from '@angular/core';
+import { HeroesModule } from './heroes/heroes.module';
 import { CentrosModule } from './centros/centros.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CentrosModule],
+  imports: [HeroesModule, CentrosModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
