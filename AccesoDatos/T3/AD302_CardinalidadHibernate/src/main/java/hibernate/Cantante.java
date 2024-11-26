8a69f44f6ab91f631d04cee46c422819625b1436
Oscar Montero <94902365+OscarW1ld3@users.noreply.github.com>
2024-11-26 17:40:48 +0100

    a

--- AccesoDatos/T3/AD302_CardinalidadHibernate/src/main/java/hibernate/Cantante.java
+++ AccesoDatos/T3/AD302_CardinalidadHibernate/src/main/java/hibernate/Cantante.java
@@ -4,63 +4,72 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "cantantes")
-public class Cantante {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int codigo;
+public class Cantante
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
 
-    @Column(name = "nombre", nullable = false)
-    private String nombre;
+	@Column(name = "nombre", nullable = false)
+	private String nombre;
 
-    @Column(name = "anio", nullable = false)
-    private int anio;
+	@Column(name = "anio", nullable = false)
+	private int anio;
 
-    @Column(name = "album", nullable = false)
-    private String album;
+	@Column(name = "album", nullable = false)
+	private String album;
 
-    @ManyToOne
-    @JoinColumn(name = "genero_id", nullable = false)
-    private GeneroMusical genero;
+	@ManyToOne
+	@JoinColumn(name = "genero_id", nullable = false)
+	private GeneroMusical genero;
 
-    // Getters y Setters
-    public int getCodigo() {
-        return codigo;
-    }
+	public int getCodigo()
+	{
+		return codigo;
+	}
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
+	public void setCodigo(int codigo)
+	{
+		this.codigo = codigo;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre()
+	{
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
 
-    public int getAnio() {
-        return anio;
-    }
+	public int getAnio()
+	{
+		return anio;
+	}
 
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
+	public void setAnio(int anio)
+	{
+		this.anio = anio;
+	}
 
-    public String getAlbum() {
-        return album;
-    }
+	public String getAlbum()
+	{
+		return album;
+	}
 
-    public void setAlbum(String album) {
-        this.album = album;
-    }
+	public void setAlbum(String album)
+	{
+		this.album = album;
+	}
 
-    public GeneroMusical getGenero() {
-        return genero;
-    }
+	public GeneroMusical getGenero()
+	{
+		return genero;
+	}
 
-    public void setGenero(GeneroMusical genero) {
-        this.genero = genero;
-    }
+	public void setGenero(GeneroMusical genero)
+	{
+		this.genero = genero;
+	}
 }
-
--- AccesoDatos/T3/AD302_CardinalidadHibernate/src/main/java/hibernate/GeneroMusical.java
+++ AccesoDatos/T3/AD302_CardinalidadHibernate/src/main/java/hibernate/GeneroMusical.java
@@ -5,39 +5,45 @@ import java.util.List;
 
 @Entity
 @Table(name = "genero_musical")
-public class GeneroMusical {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+public class GeneroMusical
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    @Column(name = "nombre", nullable = false)
-    private String nombre;
+	@Column(name = "nombre", nullable = false)
+	private String nombre;
 
-    @OneToMany(mappedBy = "genero", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Cantante> cantantes;
+	@OneToMany(mappedBy = "genero", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Cantante> cantantes;
 
-    // Getters y Setters
-    public int getId() {
-        return id;
-    }
+	public int getId()
+	{
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id)
+	{
+		this.id = id;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre()
+	{
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
 
-    public List<Cantante> getCantantes() {
-        return cantantes;
-    }
+	public List<Cantante> getCantantes()
+	{
+		return cantantes;
+	}
 
-    public void setCantantes(List<Cantante> cantantes) {
-        this.cantantes = cantantes;
-    }
+	public void setCantantes(List<Cantante> cantantes)
+	{
+		this.cantantes = cantantes;
+	}
 }
