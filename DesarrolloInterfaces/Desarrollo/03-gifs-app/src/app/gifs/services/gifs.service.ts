9f638bd69741d3a77be9f49a7187b03e752efb50
Oscar Montero <wilde.txt@gmail.com>
2025-01-16 21:19:13 +0100

    a

--- DesarrolloInterfaces/Desarrollo/03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ DesarrolloInterfaces/Desarrollo/03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -12,10 +12,28 @@ export class GifsService {
   }
 
   buscarEtiqueta(etiqueta: string): void {
+    // Quitamos espacios a la derecha y a la izquierda
+    etiqueta = etiqueta.trim();
+
+    // Evitamos busquedas vacías
     if(!etiqueta) {
       return;
     }
+
+    // Comprobamos si el array ya contiene la etiqueta, si es asi la quitamos.
+    //Esta comprobacion es case insensitive, osea si el array tiene DBZ y ponemos dbz las considera iguales, y la elimina.
+    this._historialEtiquetas.forEach((e)=>{
+      if(e.toLowerCase() === etiqueta.toLocaleLowerCase()) {
+        this._historialEtiquetas.splice(this._historialEtiquetas.indexOf(e), 1);
+      }
+    })
+
+    // limita el array a 10, todo lo que lo sobrepase lo elimina del array
+    this._historialEtiquetas.splice(9);
+
+    // Añadimos la nueva etiqueta
     this._historialEtiquetas.unshift(etiqueta);
+
     console.log(this.historialEtiquetas);
   }
 
